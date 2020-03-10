a59792291ba93729cb331f8559e6eb0ab1e52b0d
QinyunLin <linqinyu@msu.edu>
2020-03-10 00:29:00 -0500

    better visualize the comparisons between real, counterfactual 1 and counterfactual 2

--- R/check-min-dist-reshuffled.R
+++ R/check-min-dist-reshuffled.R
@@ -6,15 +6,6 @@ library(tmaptools)
 result <- st_read("data-shared/min_dist_real_reshuffled.shp") %>% 
   st_set_crs(32616)
 
-tm_shape(result) +
-  tm_polygons("real_bup")
-
-tm_shape(result) +
-  tm_polygons("s1_bup")
-
-tm_shape(result) +
-  tm_polygons("s2_bup")
-
 tmap_mode(mode = "view")
 # tmap mode set to interactive viewing 
 tm_shape(zips_IL) + tm_polygons("ratio")
@@ -26,21 +17,21 @@ result <- right_join(atrisk_IL, result, by = c("GEOID" = "zipcode")) %>%
 
 result$X <- NULL
 
-tmap_mode("view")
-tm_shape(result) +
-  tm_polygons(c("real_bup","s1_bup","s2_bup", "ratio"), style = "quantile") +
-  tm_facets(sync = TRUE, ncol = 2)
-
 tmap_mode("plot")
+tm_shape(illinois) +
+tm_borders() + 
 tm_shape(result) +
+  tm_basemap("Stamen.Watercolor") +
   tm_polygons(c("real_meth","s1_meth","s2_meth"), 
-              title = c("Real","Scenario 1","Scenario 2"),
+              title = "Distance (miles)",
               style = "fixed", breaks = c(0, 2, 10, 100), palette = "Set1") +
   tm_facets(sync = TRUE, ncol = 3) + 
-  tm_layout(legend.position = c("RIGHT", "BOTTOM"), 
+  tm_layout(panel.show = T,
+            panel.labels = c("Real","Counterfactual 1","Counterfactual 2"),
+            legend.outside = T,
             main.title = "Minimum Distance (miles) to MOUD - Methadone")
 
-tm_shape(result) +
+  tm_shape(result) +
   tm_polygons(c("real_bup","s1_bup","s2_bup"),
               title = c("Real","Scenario 1","Scenario 2"),
               style = "fixed", breaks = c(0, 5, 20, 100), palette = "Set1") +
